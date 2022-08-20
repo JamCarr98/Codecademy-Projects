eb1d0079e59ff845c68f4035181f310c4456be10
Jamie Carr <95095432+JamCarr98@users.noreply.github.com>
2022-08-20 17:09:00 +0100

    Create ufo_functions.hpp

---
+++ c++/ufo/ufo_functions.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+void display_misses(int misses);
+
+// Declare functions
+void greet();
+
+void end_game(std::string codeword, std::string answer);
+
+void player_turn();
+
+void display_status(std::vector<char> incorrect, std::string answer);
